2e79cb014e875ccfd1019e78eda0990890aa2341
Ankit Ranjan <112964212+axankit@users.noreply.github.com>
2023-08-20 01:22:01 +0530

    build the responsive navbar

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 let openBtn = document.querySelector(".bi-list");
 let closeBtn = document.querySelector(".bi-x-circle");
-// let menu = document.querySelector(".navbar__menu");
+let menu = document.querySelector(".navbar__menu");
 
 openBtn.addEventListener("click", () => {
   openBtn.classList.toggle("hide");
   closeBtn.classList.toggle("hide");
-  // menu.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
 });
 
 closeBtn.addEventListener("click", () => {
   openBtn.classList.toggle("hide");
   closeBtn.classList.toggle("hide");
-  // menu.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
 });
